f86dac1c09d260baae968e3ea8c3736850b3986d
Alimova Julia <markiza1014@gmail.com>
2023-05-18 21:18:38 +0300

    dashboard lesson+HW

--- src/route/index.js
+++ src/route/index.js
@@ -1076,6 +1076,13 @@ router.get('/slack', function (req, res) {
 
 // ================================================================
 
+router.get('/dashboard', function (req, res) {
+  res.render('dashboard', {
+    layout: null,
+  })
+})
+// ================================================================
+
 
 // Підключаємо роутер до бек-енду
 module.exports = router
